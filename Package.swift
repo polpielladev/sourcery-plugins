726b1f91875608fc726ae456d9364bd243b51b7c
Pol Piella Abadia <hi@polpiella.dev>
2022-08-30 18:03:36 +0100

    Adds `SourceryCommand` to product declaration list

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SourceryPlugins",
     products: [
+        .plugin(name: "SourceryCommand", targets: ["SourceryCommand"])
     ],
     targets: [
         .plugin(
