9e487361a3e879ca57333fdb41d239a3e65d143b
Pol Piella Abadia <hi@polpiella.dev>
2022-08-30 17:48:22 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+    name: "SourceryPlugins",
+    products: [
+    ],
+    targets: [
+        .target(
+            name: "SourceryPlugins",
+            dependencies: [])
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# SourceryPlugins
+
+A description of this package.
