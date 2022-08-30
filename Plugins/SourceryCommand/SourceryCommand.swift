f8bc5e3915c6a815936ed9b46b9d4aaa4f34d7ec
Pol Piella Abadia <hi@polpiella.dev>
2022-08-30 17:58:34 +0100

    Ensure that a configuration file is always present

--- Plugins/SourceryCommand/SourceryCommand.swift
+++ Plugins/SourceryCommand/SourceryCommand.swift
@@ -3,6 +3,10 @@ import Foundation
 
 struct SourceryCommand: CommandPlugin {
     func performCommand(context: PluginContext, arguments: [String]) async throws {
-        
+        let configFilePath = context.package.directory.appending(subpath: ".sourcery.yml").string
+        guard FileManager.default.fileExists(atPath: configFilePath) else {
+            Diagnostics.error("Could not find config at: \(configFilePath)")
+            return
+        }
     }
 }
