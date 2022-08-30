01c564df9169e93933838e93b344635a65c572e1
Pol Piella Abadia <hi@polpiella.dev>
2022-08-30 17:54:02 +0100

    Adds permission statement to the plugin definition

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .plugin(
             name: "SourceryCommand",
             capability: .command(
-                intent: .custom(verb: "sourcery-code-generation", description: "Generates Swift files from a given set of inputs")
+                intent: .custom(verb: "sourcery-code-generation", description: "Generates Swift files from a given set of inputs"),
+                permissions: [.writeToPackageDirectory(reason: "Need access to the package directory to generate files")]
             )
         )
     ]
